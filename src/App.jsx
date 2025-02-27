3aca370c6d98754a690e0209ae833a114a1eae2f
tmd <96tmdqh@gmail.com>
2025-02-27 12:09:30 +0900

    Type : Design
    css 기본 설정 초기화 cdn 추가 및 css 변수 값 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import './App.css';
-import AppLayout from "./componenets/common/AppLayout";
+import AppLayout from "./components/common/AppLayout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Benefit from "./pages/card/Benefit";
 import Mypage from "./pages/mypage/Mypage";
@@ -13,14 +13,14 @@ function App() {
 
   return (
     <BrowserRouter>
-    <AppLayout>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/card" element={<Benefit />} />
-        <Route path="/mypage" element={<Mypage />} />
-      </Routes>
-    </AppLayout>
-  </BrowserRouter>
+      <AppLayout>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/card" element={<Benefit />} />
+          <Route path="/mypage" element={<Mypage />} />
+        </Routes>
+      </AppLayout>
+    </BrowserRouter>
   );
 }
 
