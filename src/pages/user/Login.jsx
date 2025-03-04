212b70db41aa5a11b463ffce46381f5270bd0dc0
uchong <dmlcksghd@gmail.com>
2025-03-04 22:10:12 +0900

    Type: Feat
    Body: 로그인 기능
    - 카카오 네이버 로고 추가
    
    Footer:
     Resolves:
     Related to:

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -80,8 +80,16 @@ function Login() {
                     <Link to="/signup" className="signup-button">회원가입</Link>
                 </div>
                 <div className="social-login">
-                    <button className="kakao-login">카카오톡으로 계속하기</button>
-                    <button className="naver-login">네이버로 계속하기</button>
+                    <button className="kakao-login">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/e/e3/KakaoTalk_logo.svg"
+                             alt="Kakao" className="social-logo" />
+                        카카오톡으로 계속하기
+                    </button>
+                    <button className="naver-login">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/2/23/Naver_Logotype.svg"
+                             alt="Naver" className="social-logo" />
+                        네이버로 계속하기
+                    </button>
                 </div>
             </div>
         </div>
