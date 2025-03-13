4e2523f27413b9ce60edf9dc96d298e054c2ef19
chocolatestain <114946845+chocolatestain@users.noreply.github.com>
2025-03-13 12:36:13 +0900

    checkout

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import Layout from "./components/dashboard/DashboardLayout";
 import SearchResults from "./pages/admin/searchresult";
 import Detail from "./pages/admin/Detail";
  
-import Dashboard from "./pages/admin/dashboard";
+import Dashboard from "./pages/admin/Dashboard";
 import Design from "./pages/card/CardDesignPage";
 import BenefitCompare from "pages/card/BenefitCompare";
 
--- src/pages/admin/dashboard.jsx
+++ src/pages/admin/dashboard.jsx
@@ -309,4 +309,4 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard; 
+export default Dashboard;
